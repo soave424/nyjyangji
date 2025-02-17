f971fe6f1bc172a6472ee0af4074b3d368d17550
soave424 <soave424@gmail.com>
2025-02-18 01:11:10 +0900

    sidebar

---
+++ home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("남양주양지초등학교")
+with st.sidebar:
+    choice = option_menu("Menu", ["페이지1", "페이지2", "페이지3"],
+                         icons=['house', 'kanban', 'bi bi-robot'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+        "container": {"padding": "4!important", "background-color": "#fafafa"},
+        "icon": {"color": "black", "font-size": "25px"},
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#fafafa"},
+        "nav-link-selected": {"background-color": "#08c7b4"},
+    }
+    )
--- streamlit_app.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-
-st.title("남양주양지초등학교")
