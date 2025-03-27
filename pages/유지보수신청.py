0fbc5e65b70f59d11b3853252d262cb6f096b259
soave424 <soave424@gmail.com>
2025-03-27 08:03:09 +0000

    a

--- pages/유지보수신청.py
+++ pages/유지보수신청.py
@@ -6,6 +6,9 @@ st.set_page_config(layout="wide",  initial_sidebar_state="collapsed")
 def main():
     st.title('남양주양지초 2025 유지보수 신청')
 
+    st.markdown("###유지보수 신청을 구글 시트 작성으로 바꾸었습니다. 순서대로 작성해주세요."
+
+                )
     # 제공된 구글 시트 링크
     sheet_url = 'https://docs.google.com/spreadsheets/d/1jZWdoiz9J7WSHSrwgYSDjNqmOqPe0u52ZPlON6mEH9w/edit?usp=sharing'
 
