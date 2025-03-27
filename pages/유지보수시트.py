9b1a2a6755ba0f70c44db6af64fcd4c1c63c402a
soave424 <soave424@gmail.com>
2025-03-27 07:54:43 +0000

    a

--- pages/유지보수시트.py
+++ pages/유지보수시트.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 
 def main():
-    st.title('구글 시트 보기')
+    st.title('남양주양지초 2025 유지보수 신청')
 
     # 제공된 구글 시트 링크
     sheet_url = 'https://docs.google.com/spreadsheets/d/1jZWdoiz9J7WSHSrwgYSDjNqmOqPe0u52ZPlON6mEH9w/edit?usp=sharing'
 
     # IFRAME으로 구글 시트 임베드
-    st.components.v1.iframe(sheet_url, height=600, scrolling=True, width=800)
+    st.components.v1.iframe(sheet_url, height=600, scrolling=True, width=100 %)
 
 
 if __name__ == '__main__':
